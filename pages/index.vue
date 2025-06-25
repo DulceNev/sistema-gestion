4872f3ce5ab7795f793155e5c2c673e3e5b841c2
Dulce Nevarez <dulcenev04@gmail.com>
2025-06-25 13:17:16 -0500

    estructura basica del escritorio + se agrego daisyUI + se agrego la fuente correctamente

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,60 @@
 <template>
-  <div>helou</div>
+  <div class="background-image p-5">
+    <div class="grid grid-cols-9 h-screen items-start">
+      <div class="flex flex-col gap-6">
+        <div class="flex flex-col justify-center items-center">
+          <img
+            src="../public//img/icons/CARPETAAZUL.png"
+            class="icon-image"
+            alt=""
+          />
+          <span>CARPETA</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <img
+            src="../public//img/icons/CARPETAMORADA.png"
+            class="icon-image"
+            alt=""
+          />
+          <span>CARPETA</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <img
+            src="../public/img/icons/POMODORO.png"
+            class="icon-image"
+            alt=""
+          />
+          <span>POMODORO</span>
+        </div>
+
+        <div class="flex flex-col justify-center items-center">
+          <img src="../public//img/icons/TODO.png" class="icon-image" alt="" />
+          <span>TODO</span>
+        </div>
+      </div>
+      <div class="flex flex-col gap-6">
+        <div class="flex flex-col justify-center items-center">
+          <img
+            src="../public//img/icons/CARPETAROSA.png"
+            class="icon-image"
+            alt=""
+          />
+          <span>CARPETA</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <img
+            src="../public//img/icons/CALENDARIO.png"
+            class="icon-image"
+            alt=""
+          />
+          <span>CALENDARIO</span>
+        </div>
+
+        <div class="flex flex-col justify-center items-center">
+          <img src="../public//img/icons/NOTES.png" class="icon-image" alt="" />
+          <span>NOTAS</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
